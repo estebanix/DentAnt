3993f55a965f69b0355dd0c00efb8542697f213b
estebanix <dakisunshine@gmail.com>
2023-09-27 15:57:44 +0200

    First version Article

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,9 +1,19 @@
-import styles from "./Article.module.scss"
+import React from 'react';
+import styles from './Article.module.scss';
 
-export const Article = () => {
+interface ArticleProps {
+    title: string;
+    text: string;
+    right: boolean;
+}
+
+export const Article: React.FC<ArticleProps> = ({ title, text, right }) => {
+    const containerClassName = right ? styles.articleContainer + ' ' + styles.right : styles.articleContainer + ' ' + styles.left;
+
     return (
-        <div className={styles.articleContainer}>
-
+        <div className={containerClassName}>
+            <h2 className={styles.articleTitle}>{title}</h2>
+            <p className={styles.articleParag}>{text}</p>
         </div>
     );
-}
+};
--- src/containers/HomePage.tsx/HomePage.tsx
+++ src/containers/HomePage.tsx/HomePage.tsx
@@ -1,13 +1,19 @@
 import { HeaderMenu } from "../../components/HeaderMenu";
 import { Article } from "../../components/Article";
+import texts from "./../../../public/locales/texts.json";
 
 import styles from "./HomePage.module.scss";
 
 export const HomePage = () => {
+    const { homePageMainTitle, homePageMainText } = texts;
     return(
         <div className={styles.homePageContainer}>
             <HeaderMenu />
-            <Article />
+            <Article 
+                title={homePageMainTitle} 
+                text={homePageMainText} 
+                right={false}
+            />
         </div>
     );
 }
