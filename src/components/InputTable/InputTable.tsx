a8f718856a84791250b31d4e0596beeb4fd18755
estebanix <dakisunshine@gmail.com>
2024-02-17 16:45:08 +0100

    firtst somehow looking version of inpiut table

--- src/components/InputTable/InputTable.tsx
+++ src/components/InputTable/InputTable.tsx
@@ -1,50 +1,117 @@
 import { useState } from "react";
-import Spreadsheet, {
-  createFormulaParser,
-  Matrix,
-  CellBase,
-} from "react-spreadsheet";
-import { Button } from "../Button";
+import styles from "./InputTable.module.scss";
 
-interface InputTableProps {}
+type DataArray = (string | number)[][];
 
-export const InputTable: React.FC<InputTableProps> = () => {
-  const [data, setData] = useState<Matrix<CellBase>>([
-    [{ value: "2" }],
-    [{ value: "2" }],
+const colArr = [
+  "18",
+  "17",
+  "16",
+  "15",
+  "14",
+  "13",
+  "12",
+  "11",
+  "21",
+  "22",
+  "23",
+  "24",
+  "25",
+  "26",
+  "27",
+  "28"
+];
+const dentalParameters = [
+  "Tooth presence, absence, carious",
+  "Occlusal surface caries (fissure, groove, fossa sites)",
+  "Pit caries",
+  "Occlusal attrition score (Smith 1984)",
+  "Occlusal attrition facet dentine caries",
+  "Attrition facet enamel rim chipping/caries",
+  "Mesial attrition score",
+  "Mesial contact area caries",
+  "Mesial root surface caries",
+  "Mesial root exposure, CEJ-AC (mm)",
+  "X. Mesial dental calculus presence",
+  "Distal attrition score",
+  "Distal contact point caries",
+  "Distal root surface caries",
+  "Distal root exposure, CEJ-AC (mm)",
+  "X. Distal dental calculus presence",
+  "Buccal smooth surface enamel caries",
+  "Buccal root surface caries",
+  "Buccal root exposure, CEJ-AC (mm)",
+  "X. Buccal dental calculus presence",
+  "Palatinal smooth surface, enamel caries",
+  "Palatinal root surface caries",
+  "Palatinal root exposure, CEJ-AC (mm)",
+  "X. Palatinal dental calculus presence",
+  "DDE in the occlusal region",
+  "DDE in the contact area",
+  "DDE lower down crown side or in cervical area",
+  "Deformities of the alveolar process (Karn et al., 1984)",
+  "Periodontal status (Kerr 1991)",
+];
+
+const initialRows = dentalParameters.length;
+const initialCols = 17;
+
+console.log(colArr.length);
+
+const rowNames = Array.from({ length: 29 }, (_, index) => dentalParameters[index]);
+
+const colNames = Array.from({ length: 17 }, (_, index) => colArr[index]);
+
+export const InputTable = () => {
+  const createEmptyRow = () => Array(initialCols).fill("");
+  const [data, setData] = useState<DataArray>(() => [
+    colNames,
+    ...Array(initialRows).fill(0).map(createEmptyRow),
   ]);
-  const customCreateFormulaParser = (data: Matrix<CellBase>) =>
-    createFormulaParser(data, { SUM: undefined });
 
-  const columnLabels = ["Number"];
-  const rowLabels = ["Item 1", "Item 2"];
-
-  const [sum, setSum] = useState<string | number>("");
-
-  const makeSum = () => {
-    const sums = data
-      .flat()
-      .map((dat) => parseFloat(dat?.value || "0"))
-      .reduce((acc, curr) => acc + curr, 0);
-
-    setSum(isNaN(sums) ? "Use numbers!" : sums);
+  const handleChange = (rowIndex: number, colIndex: number, value: string) => {
+    const newValue = value !== "" ? parseInt(value, 10) : "";
+    const newData: DataArray = data.map((row, i) =>
+      i === rowIndex
+        ? row.map((cell, j) => (j === colIndex ? newValue : cell))
+        : row
+    );
+    setData(newData);
   };
-  
 
   return (
-    <>
-      <Spreadsheet
-        data={data}
-        columnLabels={columnLabels}
-        rowLabels={rowLabels}
-        createFormulaParser={customCreateFormulaParser}
-        onChange={(e) => {
-          if (e) setData(e), console.log(e);
-        }}
-        onSelect={(e) => console.log(e)}
-      />
-      <Button text="Sum" colorVariant="primary" reaction={makeSum} />
-      <p>sum is: {sum}</p>
-    </>
+    <div className={styles.inputTable}>
+      <table className={styles.table}>
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((cell, colIndex) => (
+                <td key={colIndex} className={styles.cell}>
+                  {rowIndex === 0 && colIndex > 0 ? (
+                    // Column labels
+                    <label>{colNames[colIndex - 1]}</label>
+                  ) : colIndex === 0 && rowIndex > 0 ? (
+                    // Row labels
+                    <label>{rowNames[rowIndex - 1]}</label>
+                  ) : colIndex > 0 && rowIndex > 0 ? (
+                    // Input cells
+                    <label>
+                      <input
+                        type="text"
+                        value={cell.toString()}
+                        onChange={(e) =>
+                          handleChange(rowIndex, colIndex, e.target.value)
+                        }
+                        className={styles.input}
+                      />
+                    </label>
+                  ) : null}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
